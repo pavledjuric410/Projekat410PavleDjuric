a5de4581e0f8e4547b4d7643bae5822b44f34a79
pavledjuric410 <pavle.djuric@prvabeogim.edu.rs>
2026-04-01 09:13:31 +0000

    Update Login.Designer.cs

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -29,21 +29,123 @@ namespace Projekat410PavleDjuric
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtlozinka = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonregistracija = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lozinka";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(374, 215);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(153, 39);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "LOGIN";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(354, 149);
+            this.txtemail.Multiline = true;
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(200, 23);
+            this.txtemail.TabIndex = 3;
+            // 
+            // txtlozinka
+            // 
+            this.txtlozinka.Location = new System.Drawing.Point(354, 178);
+            this.txtlozinka.Multiline = true;
+            this.txtlozinka.Name = "txtlozinka";
+            this.txtlozinka.Size = new System.Drawing.Size(200, 22);
+            this.txtlozinka.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(362, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dobrodosli!";
+            // 
+            // buttonregistracija
+            // 
+            this.buttonregistracija.Location = new System.Drawing.Point(687, 215);
+            this.buttonregistracija.Name = "buttonregistracija";
+            this.buttonregistracija.Size = new System.Drawing.Size(172, 39);
+            this.buttonregistracija.TabIndex = 6;
+            this.buttonregistracija.Text = "REGISTRACIJA";
+            this.buttonregistracija.UseVisualStyleBackColor = true;
+            this.buttonregistracija.Click += new System.EventHandler(this.buttonregistracija_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(117, 232);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Items.AddRange(new object[] {
+            "skola",
+            "kuca"});
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(896, 412);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonregistracija);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtlozinka);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtlozinka;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonregistracija;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
-
