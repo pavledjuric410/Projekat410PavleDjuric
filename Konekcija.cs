a1c5c1fbc4e6f9d48c22212aa51712e1651f4a1d
pavledjuric410 <pavle.djuric@prvabeogim.edu.rs>
2026-04-01 09:12:21 +0000

    Add files via upload

---
+++ Konekcija.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Projekat_2026_Nebojsa_Matic
+{
+    internal class Konekcija
+    {
+        static public SqlConnection Connect(string gde)
+        {
+            string CS;
+            CS = ConfigurationManager.ConnectionStrings[gde].ConnectionString;
+            return new SqlConnection(CS);
+        }
+    }
+}
