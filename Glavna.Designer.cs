b97b70dfeb9d1940a9b849f3316279583bc4b82c
pavledjuric410 <pavle.djuric@prvabeogim.edu.rs>
2026-04-01 09:09:54 +0000

    Add files via upload

---
+++ Glavna.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Projekat_2026_Nebojsa_Matic
+{
+    partial class Glavna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dokumentacijaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.podaciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izvestajiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administracijaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dokumentacijaToolStripMenuItem,
+            this.podaciToolStripMenuItem,
+            this.izvestajiToolStripMenuItem,
+            this.administracijaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dokumentacijaToolStripMenuItem
+            // 
+            this.dokumentacijaToolStripMenuItem.Name = "dokumentacijaToolStripMenuItem";
+            this.dokumentacijaToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.dokumentacijaToolStripMenuItem.Text = "Dokumentacija";
+            // 
+            // podaciToolStripMenuItem
+            // 
+            this.podaciToolStripMenuItem.Name = "podaciToolStripMenuItem";
+            this.podaciToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.podaciToolStripMenuItem.Text = "Podaci";
+            // 
+            // izvestajiToolStripMenuItem
+            // 
+            this.izvestajiToolStripMenuItem.Name = "izvestajiToolStripMenuItem";
+            this.izvestajiToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.izvestajiToolStripMenuItem.Text = "Izvestaji";
+            // 
+            // administracijaToolStripMenuItem
+            // 
+            this.administracijaToolStripMenuItem.Name = "administracijaToolStripMenuItem";
+            this.administracijaToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.administracijaToolStripMenuItem.Text = "Administracija";
+            // 
+            // Glavna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Glavna";
+            this.Text = "Glavna";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Glavna_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dokumentacijaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem podaciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izvestajiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administracijaToolStripMenuItem;
+    }
+}
